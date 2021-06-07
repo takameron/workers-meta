0d025a3c7da2d26d22f5adb2c45f22871068e4af
takameron <tech@takameron.info>
2021-06-07 15:42:05 +0900

    v1.0.0

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,59 @@
+import * as chardet from 'chardet';
+import * as iconv from 'iconv-lite';
+
+export async function handleOptions(request: Request): Promise<Response> {
+  return new Response(null, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,OPTIONS',
+      'Access-Control-Max-Age': '86400',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  })
+}
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  const { searchParams } = new URL(request.url)
+  let targetURL: string = String(searchParams.get('url'))
+
+  // Add 'http://' if there is no http:// or https://
+  const hitProtocol: RegExp = /^https?:\/\//
+  if (!hitProtocol.test(targetURL)) {
+    targetURL = "http://" + targetURL
+  }
+
+  let res: { [key: string]: string; } = {};
+
+  const response: Response = await fetch(targetURL)
+  if (response.body) {
+    const arraybuffer: ArrayBuffer = await response.arrayBuffer()
+    const array: Uint8Array = new Uint8Array(arraybuffer)
+
+    // Decode
+    const encoding: string = chardet.detect(array)!.toString()
+    if (!encoding) throw new Error();
+    const html: string = iconv.decode(array, encoding);
+
+    // Analysis of meta tags
+    const head = html.match(/<head>[\s\S]*?<\/head>/i)
+    if (head) {
+      const tags = head[0].match(/<meta[\s\S]*?>/gi)
+      if (tags) {
+        tags.forEach(tag => {
+          const key = tag.match(/(?<=(property|name|itemprop)=")[\s\S]+?(?=")/i)
+          const value = tag.match(/(?<=content=")[\s\S]+?(?=")/i)
+          if (key && value)
+            // key is unified to lowercase
+            res[String(key[0]).toLowerCase()] = String(value[0])
+        })
+      }
+    }
+  }
+
+  const json = JSON.stringify(res)
+  return new Response(json, {
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+    },
+  })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-import { handleRequest } from './handler'
+import { handleOptions, handleRequest } from './handler'
 
 addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request))
+  const request = event.request
+
+  if (request.method === 'OPTIONS') {
+    return event.respondWith(handleOptions(request))
+  }
+
+  if (request.method !== 'GET') {
+    return event.respondWith(
+      new Response('Method Not Allowed', {
+        status: 405,
+      }),
+    )
+  }
+
+  event.respondWith(handleRequest(request))
 })
